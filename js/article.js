f54eb69cfaa1c71077bd0b235f892d68d0527163
islomumarjonov <islomjon.umarjonov2@gmail.com>
2023-07-27 13:11:16 +0500

    first version

---
+++ js/article.js
@@ -0,0 +1,48 @@
+const idEl = document.getElementById("id");
+const overlay = document.querySelector(".overlay");
+
+const qurey = window.location.search;
+const id = qurey.slice(1);
+
+const articlesContainer = document.getElementById("article-container");
+
+idEl.textContent = id;
+
+const request = new XMLHttpRequest();
+
+request.addEventListener("readystatechange", () => {
+  if (request.readyState == 4 && request.status == 200) {
+    const data = JSON.parse(request.responseText);
+    updateUI(data);
+    overlay.classList.add("hidden");
+  } else if (request.readyState == 4) {
+    console.log("error");
+    overlay.classList.add("hidden");
+  } else {
+    overlay.classList.remove("hidden");
+  }
+});
+
+request.open("GET", `http://localhost:3000/articles/${id}`);
+request.send();
+
+function updateUI(data) {
+  const ul = document.createElement("ul");
+  const docFrag = document.createDocumentFragment();
+  console.log(docFrag);
+  console.log(data);
+  const li = document.createElement("li");
+  li.classList.add("card");
+  li.innerHTML = `
+      <h3>Title: ${data.title}</h3>
+      <br />
+      <p>Author: ${data.author}</p>
+      <br />
+      <p>Body: ${data.body}</p>
+      <br />
+      <a href=${`./index.html`}>Back to main page</a>
+    `;
+  docFrag.appendChild(li);
+  ul.appendChild(docFrag);
+  articlesContainer.appendChild(ul);
+}
